99a2f805c76f9c99cb9ee4ede2c3de6210c3a730
Jakob Matthias Bek Farholt <jako741m@zbc.dk>
2021-08-31 09:11:14 +0100

    Add project files.

---
+++ Planeterne_OOP/Planeter.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planeterne_OOP
+{
+    public class Planeter
+    {
+        private string navn;
+        private double mass;
+        private double diameter;
+        private double density;
+        private double gravity;
+        private double rotation_period;
+        private double length_of_day;
+        private double distance_from_sun;
+        private double orbital_period;
+        private double orbital_velocity;
+        private int mean_temperature;
+        private int number_of_moons;
+        private bool ring_system;
+
+        public string Navn
+        {
+            get
+            {
+                return this.navn;
+            }
+            set
+            {
+                this.navn = value;
+            }
+        }
+        public double Mass
+        {
+            get
+            {
+                return this.mass;
+            }
+            set
+            {
+                this.mass = value;
+            }
+        }
+        public double Diameter
+        {
+            get
+            {
+                return this.diameter;
+            }
+            set
+            {
+                this.diameter = value;
+            }
+        }
+        public double Density
+        {
+            get
+            {
+                return this.density;
+            }
+            set
+            {
+                this.density = value;
+            }
+        }
+        public double Gravity
+        {
+            get
+            {
+                return this.gravity;
+            }
+            set
+            {
+                this.gravity = value;
+            }
+        }
+        public double Rotation_period
+        {
+            get
+            {
+                return this.rotation_period;
+            }
+            set
+            {
+                this.rotation_period = value;
+            }
+        }
+        public double Length_of_day
+        {
+            get
+            {
+                return this.length_of_day;
+            }
+            set
+            {
+                this.length_of_day = value;
+            }
+        }
+        public double Distance_from_sun
+        {
+            get
+            {
+                return this.distance_from_sun;
+            }
+            set
+            {
+                this.distance_from_sun = value;
+            }
+        }
+        public double Orbital_period
+        {
+            get
+            {
+                return this.orbital_period;
+            }
+            set
+            {
+                this.orbital_period = value;
+            }
+        }
+        public double Orbital_velocity
+        {
+            get
+            {
+                return this.orbital_velocity;
+            }
+            set
+            {
+                this.orbital_velocity = value;
+            }
+        }
+        public int Mean_temperature
+        {
+            get
+            {
+                return this.mean_temperature;
+            }
+            set
+            {
+                this.mean_temperature = value;
+            }
+        }
+        public int Number_of_moons
+        {
+            get
+            {
+                return this.number_of_moons;
+            }
+            set
+            {
+                this.number_of_moons = value;
+            }
+        }
+        public bool Ring_system
+        {
+            get
+            {
+                return this.ring_system;
+            }
+            set
+            {
+                this.ring_system = value;
+            }
+        }
+    }
+}
---
+++ Planeterne_OOP/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Planeterne_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Planeter Mercury = new Planeter();
+            {
+                Mercury.Navn = "Mercury";
+                Mercury.Mass = 0.330;
+                Mercury.Diameter = 4879;
+                Mercury.Density = 5427;
+                Mercury.Gravity = 3.7;
+                Mercury.Rotation_period = 1407.6;
+                Mercury.Length_of_day = 4222.6;
+                Mercury.Distance_from_sun = 57.9;
+                Mercury.Orbital_period = 88.0;
+                Mercury.Orbital_velocity = 47.4;
+                Mercury.Mean_temperature = 167;
+                Mercury.Number_of_moons = 0;
+                Mercury.Ring_system = false;
+            }
+
+            Planeter Venus = new Planeter();
+            {
+                Venus.Navn = "Venus";
+                Venus.Mass = 4.87;
+                Venus.Diameter = 12104;
+                Venus.Density = 5243;
+                Venus.Gravity = 8.9;
+                Venus.Rotation_period = -5832.5;
+                Venus.Length_of_day = 2802.0;
+                Venus.Distance_from_sun = 108.2;
+                Venus.Orbital_period = 224.7;
+                Venus.Orbital_velocity = 35.0;
+                Venus.Mean_temperature = 464;
+                Venus.Number_of_moons = 0;
+                Venus.Ring_system = false;
+            }
+
+            Planeter Earth = new Planeter();
+            {
+                Earth.Navn = "Earth";
+                Earth.Mass = 5.97;
+                Earth.Diameter = 12756;
+                Earth.Density = 5514;
+                Earth.Gravity = 9.8;
+                Earth.Rotation_period = 23.9;
+                Earth.Length_of_day = 24.7;
+                Earth.Distance_from_sun = 149.6;
+                Earth.Orbital_period = 365.2;
+                Earth.Orbital_velocity = 29.8;
+                Earth.Mean_temperature = 15;
+                Earth.Number_of_moons = 1;
+                Earth.Ring_system = false;
+            }
+
+            Planeter Mars = new Planeter();
+            {
+                Mars.Navn = "Mars";
+                Mars.Mass = 0.642;
+                Mars.Diameter = 6792;
+                Mars.Density = 3933;
+                Mars.Gravity = 3.7;
+                Mars.Rotation_period = 24.6;
+                Mars.Length_of_day = 24.7;
+                Mars.Distance_from_sun = 227.9;
+                Mars.Orbital_period = 687.0;
+                Mars.Orbital_velocity = 24.1;
+                Mars.Mean_temperature = -65;
+                Mars.Number_of_moons = 2;
+                Mars.Ring_system = false;
+            }
+
+            Planeter Jupiter = new Planeter();
+            {
+                Jupiter.Navn = "Jupiter";
+                Jupiter.Mass = 1898;
+                Jupiter.Diameter = 142984;
+                Jupiter.Density = 1326;
+                Jupiter.Gravity = 23.1;
+                Jupiter.Rotation_period = 9.9;
+                Jupiter.Length_of_day = 9.9;
+                Jupiter.Distance_from_sun = 778.6;
+                Jupiter.Orbital_period = 4331;
+                Jupiter.Orbital_velocity = 13.1;
+                Jupiter.Mean_temperature = -110;
+                Jupiter.Number_of_moons = 67;
+                Jupiter.Ring_system = true;
+            }
+
+            Planeter Saturn = new Planeter();
+            {
+                Saturn.Navn = "Saturn";
+                Saturn.Mass = 568;
+                Saturn.Diameter = 120536;
+
+            }
+        }
+    }
+}
